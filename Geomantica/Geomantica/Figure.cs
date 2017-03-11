3298c984d58c67d22fd8fef4b8abe0d698e022c0
Nyk Cowham <nyk@cowham.net>
2017-03-11 14:57:52 +0700

    Added Rune enum and Figure struct.

---
+++ Geomantica/Geomantica/Figure.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geomantica
+{
+    // <summary>Datatype that represents a geomantic figure</summary>
+    public struct Figure
+    {
+        public Rune Rune { get; }
+
+        // <summary>Initializes a new figure with a particular Geomantica.Rune value.</summary>
+        // <see cref="Geomantica.Rune"/>
+        // <param name="value">A Rune value of the figure</param>
+        // <returns>An instantiated Geomantica.Figure object
+        public Figure(Rune rune) : this()
+        {
+            Rune = rune;
+        }
+
+        // <summary>Overloaded + operator to conjugate two Geomantica.Figure objects.</summary>
+        // <param name="prefix">The prefixed figure</param>
+        // <param name="suffix">The suffixed figure</param>
+        // <returns>A new Geomantica.Figure object</returns>
+        public static Figure operator +(Figure prefix, Figure suffix)
+        {
+            // XOR the runes of the two figures and return new figure 
+            // initialized with resulting rune value.
+            var rune = prefix.Rune ^ suffix.Rune;
+            return new Figure(rune);
+        }
+
+        // <summary>Overloaded bitwise complement operator (~) to get the complementary Geomantica.Figure object.</summary>
+        // <para name="suffix">The Geomantica.Figure object on which the operator operates</param>
+        // <returns>A new Geomantica.Figure object</returns>
+        public static Figure operator ~(Figure suffix)
+        {
+            // Take the complement of the Rune, and subtract the first half of the byte,
+            // then return a new figure initialized with the resulting rune value.
+            var rune = ~suffix.Rune - 240;
+            return new Figure(rune);
+        }
+
+        // <summary>Override the ToString() method to return the name of the rune/figure.
+        public override string ToString() => $"{this.Rune.ToString()}";
+    }
+}
---
+++ Geomantica/Geomantica/Rune.cs
@@ -0,0 +1,23 @@
+﻿namespace Geomantica
+{
+    public enum Rune : byte
+    {
+                        // Decimal  Binary      Dots        Latin Name          English Name
+         Populus,       //  0       0000        2222        Populus             The People
+         Tristia,       //  1       0001        2221        Tristia             Sorrow
+         Albus,         //  2       0010        2212        Albus               White
+         FortunaMajor,  //  3       0011        2212        Fortuna Major       Greater Fortune
+         Rubeus,        //  4       0100        2122        Rubeus              Red
+         Acquisitio,    //  5       0101        2121        Acquisitio          Gain
+         Conjunctio,    //  6       0110        2112        Conjunctio          Conjunction
+         CaputDraconis, //  7       0111        2111        Caput Draconis      Dragon's Head
+         Laetitia,      //  8       1000        1222        Laetitia            Joy
+         Carcer,        //  9       1001        1221        Carcer              Prison
+         Admissio,      //  10      1010        1212        Admissio            Loss
+         Puella,        //  11      1011        1211        Puella              Girl
+         FortunaMinor,  //  12      1100        1122        Fortuna Minor       Lesser Fortune
+         Puer,          //  13      1101        1121        Puer                Boy
+         CaudaDraconis, //  14      1110        1112        Cauda Draconis      Dragon's Head
+         Via            //  15      1111        1111        Via                 The Way
+    }
+}
